9be706dad1c30e3a07619aa2b52c8fe9ffc58761
Rémi Bastide <Remi.Bastide@irit.fr>
2017-09-25 11:21:25 +0200

    Version initiale

---
+++ src/main/java/testingwithhsqldb/SimpleDataAccessObject.java
@@ -0,0 +1,42 @@
+package testingwithhsqldb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class SimpleDataAccessObject {
+	private final DataSource myDataSource;
+	
+	public SimpleDataAccessObject(DataSource dataSource) {
+		myDataSource = dataSource;
+	}
+
+	/**
+	 * Renvoie le nom d'un client à partir de son ID
+	 * @param id la clé du client à chercher
+	 * @return le nom du client (LastName) ou null si pas trouvé
+	 * @throws SQLException 
+	 */
+	public String nameOfCustomer(int id) throws SQLException {
+		String result = null;
+		
+		String sql = "SELECT LastName FROM Customer WHERE ID = ?";
+		try (Connection myConnection = myDataSource.getConnection(); 
+		     PreparedStatement statement = myConnection.prepareStatement(sql)) {
+			statement.setInt(1, id); // On fixe le 1° paramètre de la requête
+			try ( ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					// est-ce qu'il y a un résultat ? (pas besoin de "while", 
+                                        // il y a au plus un enregistrement)
+					// On récupère les champs de l'enregistrement courant
+					result = resultSet.getString("LastName");
+				}
+			}
+		}
+		// dernière ligne : on renvoie le résultat
+		return result;
+	}
+	
+}
---
+++ src/test/java/testingwithhsqldb/HsqlDBTest.java
@@ -0,0 +1,68 @@
+package testingwithhsqldb;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.hsqldb.cmdline.SqlFile;
+import org.hsqldb.cmdline.SqlToolError;
+
+
+public class HsqlDBTest {
+	private static DataSource myDataSource;
+	private static Connection myConnection ;
+	
+	private SimpleDataAccessObject myObject;
+	
+	@Before
+	public  void setUp() throws IOException, SqlToolError, SQLException {
+		// On crée la connection vers la base de test "in memory"
+		myDataSource = getDataSource();
+		myConnection = myDataSource.getConnection();
+		// On initialise la base avec le contenu d'un fichier de test
+		String sqlFilePath = HsqlDBTest.class.getResource("testdata.sql").getFile();
+		SqlFile sqlFile = new SqlFile(new File(sqlFilePath));
+
+		sqlFile.setConnection(myConnection);
+		sqlFile.execute();
+		sqlFile.closeReader();
+            	myObject = new SimpleDataAccessObject(myDataSource);
+
+	}
+	
+	@After
+	public void tearDown() throws IOException, SqlToolError, SQLException {
+		myConnection.close();
+             	myObject = null; // Pas vraiment utile
+
+	}
+
+	@Test
+	public void findExistingCustomer() throws SQLException {
+		String name = myObject.nameOfCustomer(0);
+		assertNotNull("Customer exists, name should not be null", name);
+		assertEquals("Bad name found !", "Steel", name);
+	}
+
+	@Test
+	public void nonExistingCustomerReturnsNull() throws SQLException {
+		String name = myObject.nameOfCustomer(-1);
+		assertNull("name should be null, customer does not exist !", name);
+	}
+
+	public static DataSource getDataSource() throws SQLException {
+		org.hsqldb.jdbc.JDBCDataSource ds = new org.hsqldb.jdbc.JDBCDataSource();
+		ds.setDatabase("jdbc:hsqldb:mem:testcase;shutdown=true");
+		ds.setUser("sa");
+		ds.setPassword("sa");
+		return ds;
+	}	
+}
